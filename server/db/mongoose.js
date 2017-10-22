ed72e1bfcf7546a6d51544bfb56519c20cacc9a6
Adil <https://_redemption@bitbucket.org/_redemption/lu-canteen.git>
2017-10-23 00:21:21 +0600

    creatd seperate database for development and testing

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MongooseApp');
-mongoose.connect('mongodb://localhost:27017/MongooseApp');
+mongoose.connect(process.env.MONGODB_URI);
+//mongoose.connect('process.env.MONGODB_URI || mongodb://localhost:27017/MongooseApp');
+//mongoose.connect('mongodb://localhost:27017/MongooseApp');
 
 module.exports = {mongoose};
