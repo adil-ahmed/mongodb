a32789b44385b19ab2f840fbf46cec022c0a9e81
Adil <https://_redemption@bitbucket.org/_redemption/lu-canteen.git>
2017-10-23 22:56:45 +0600

    Added private route and authentication middlewares

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const{ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -105,7 +106,11 @@ app.post('/users', (req,res) => {
         res.status(400).send(error);
     });
 });
+///****Private route***///
 
+app.get('/users/me',authenticate, (req,res) => {
+    res.send(req.user);
+});
 
 
 
