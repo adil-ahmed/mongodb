554efa87d4020553640a2031fa64c1046ba858ce
Adil <https://_redemption@bitbucket.org/_redemption/lu-canteen.git>
2017-10-23 21:18:15 +0600

    added Post users route and token method

--- server/server.js
+++ server/server.js
@@ -92,6 +92,20 @@ app.patch('/todos/:id',(req,res) => {
     });
 });
 
+/////***User post***////
+app.post('/users', (req,res) => {
+    var body = _.pick(req.body, ['email','password']);
+    var user = new User(body);
+    user.save().then(() => {
+        //res.send(user);
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((error) => {
+        res.status(400).send(error);
+    });
+});
+
 
 
 
