e3b60391488a7f3c9b4dccde49cc13e9d27911c2
Adil <https://_redemption@bitbucket.org/_redemption/lu-canteen.git>
2017-10-26 00:28:08 +0600

    modified config file and added config.json

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,19 @@
 const env = process.env.NODE_ENV || 'development';
-
-if(env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/MongooseApp'; 
-}
-else if(env === 'test')
+if(env === 'development' || env === 'test')
 {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/MongooseAppTest';
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
+// if(env === 'development') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/MongooseApp'; 
+// }
+// else if(env === 'test')
+// {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/MongooseAppTest';
+// }
